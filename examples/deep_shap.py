fix normalization in expected ig and update shap value imag

examples/deep_shap.py
@@ -37,12 +37,42 @@ def shap_numy(path, all_classes, model, baseline_type):
                 background = np.array([tf.keras.preprocessing.image.img_to_array(background_obj)/normalized_factor])
                 e = shap.DeepExplainer(model, background)
                 shap_values = e.shap_values(imag)
-                output_path = os.path.join(base_image_path,  "output", "shap")
+
+                output_path = os.path.join(base_image_path,  "output", "shap", baseline_type)
                 if not os.path.exists(output_path):
                     os.makedirs(output_path)
                 f_in_path = os.path.join(output_path, f"{file_name}_baseline_{j}.npy")
                 np.save(f_in_path, shap_values)
+                image_path = os.path.join(output_path, f"{file_name}_baseline_{j}.png")
+                shap.image_plot(shap_values, -imag, path=image_path)
+                image_path_1 = os.path.join(output_path, f"{file_name}_baseline_{j}_v1.png")                
+                shap.image_plot([shap_values[i], shap_values[j]], -imag, path=image_path_1)
+                image_path_2 = os.path.join(output_path, f"{file_name}_baseline_{j}_v2.png")
+                shap.image_plot([shap_values[i]- shap_values[j]], -imag, path=image_path_2)
 
+
+
+def shap_image(target_label, baseline_label, base_dir, output_path):
+    # shap_path = f"/Users/pin-jutien/tfds-download/from_ss/shap2/mnist/{target_label}/transform_to_{baseline_label}/output/explains/numpy/shap/{target_label}_shap.npy"
+
+    shap_values = np.load(shap_path)[:, 0, :, :, :]
+    input_image_path = f"/Users/pin-jutien/tfds-download/from_ss/shap2/mnist/{target_label}/{target_label}.png"
+    background_image_path = f"/Users/pin-jutien/tfds-download/from_ss/shap2/mnist/{target_label}/transform_to_{baseline_label}/{target_label}_transformed.png"
+    if c == 1:
+        imag_obj = tf.keras.preprocessing.image.load_img(input_image_path, target_size=(h, w), color_mode="grayscale")                           
+        background_obj = tf.keras.preprocessing.image.load_img(background_image_path, target_size=(h, w), color_mode="grayscale")                
+    else:                                                                                                                                        
+        imag_obj = tf.keras.preprocessing.image.load_img(input_image_path, target_size=(h, w))                                                   
+        background_obj = tf.keras.preprocessing.image.load_img(background_image_path, target_size=(h, w))                                        
+    imag = np.array([tf.keras.preprocessing.image.img_to_array(imag_obj)/normalized_factor])                                                     
+    background = np.array([tf.keras.preprocessing.image.img_to_array(background_obj)/normalized_factor]) 
+    num_class = shap_values.shape[0]
+    shap_values = [shap_values[i].reshape(1, 28, 28, 1) for i in range(num_class)]
+    # imag_path = f"/Users/pin-jutien/tfds-download/from_ss/shap/mnist/shap_value/result/"
+    imag_path = os.path.join(output_path, "image",f"shap_target_{target_label}_baseline_{baseline_label}.png")
+    np_path = os.path.join(output_path, "numpy",f"shap_target_{target_label}_baseline_{baseline_label}.npy")
+    np.save(np_path, np.array(shap_values))
+    shap.image_plot(shap_values, -imag, path=imag_path)
 
 if __name__ == '__main__':
     model = tf.keras.models.load_model("/home/ptien/from_ss/mnist/model_mnist_keras.h5")
@@ -58,4 +88,5 @@ if __name__ == '__main__':
     # path = "/home/ptien/from_ss/mnist1_s"
     # path = "/home/ptien/from_ss/mnist1_rs/"
     shap_numy(path, all_classes, model, baseline_type)
-
+    # output_path = os.path.join(path, "shap", "plot")
+    # shap_image(target_label, baseline_label, output_path)

examples/explain_helper.py
@@ -74,10 +74,10 @@ def explain_model(model_path,
                 output[type_] = attributions_base_
             elif type_ == "expected_intgrad":
                 preds = model.predict(xs)
-                ys = [np.argmax(preds[i]) for i in range(len(preds))]
-                ys = tf.keras.utils.to_categorical(ys, num_class)
+                ys_ = [np.argmax(preds[i]) for i in range(len(preds))]
+                ys_1 = tf.keras.utils.to_categorical(ys_, num_class)
                 base_for_exp_ig = get_baseline_data(data_type, y_label, subsample=steps)
-                attributions_ = de.explain("intgrad", target_tensor, input_tensor, xs, ys=ys,
+                attributions_ = de.explain("intgrad", target_tensor, input_tensor, xs, ys=ys_1,
                                            baseline=base_for_exp_ig, steps=steps, stochastic_mask_flag=type_)
                 output[type_] = attributions_
             elif type_ == "occlusion":

examples/get_labels_data.py
@@ -2,11 +2,11 @@ import tensorflow_datasets as tfds
 import numpy as np
 
 def get_baseline_data(data_type, y_label, subsample=200):
-    # uppermax = int(subsample*20)
+    uppermax = int(subsample*20)
     ds = tfds.load(data_type, split='train').prefetch(1)
     dslist = np.array(list(tfds.as_numpy(ds)))
     nrows = len(dslist)
-    # m = np.random.choice(nrows, uppermax, replace=False)
+    m = np.random.choice(nrows, uppermax, replace=False)
     # dslist = dslist[m]
     res = []
     for label in y_label:
@@ -14,7 +14,7 @@ def get_baseline_data(data_type, y_label, subsample=200):
         for ds in dslist:
             if ds.get("label") == label:
                 base_img_all += [ds.get("image")]
-        base_img_all = np.array(base_img_all)
+        base_img_all = np.array(base_img_all)/255.
         N = base_img_all.shape[0]
         m = np.random.choice(N, subsample, replace=False)
         res +=[ base_img_all[m]]